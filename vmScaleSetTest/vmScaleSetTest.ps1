a7f93a6efa5579e15556530e123d798b25b6e439
Jim Davis <jimdavis@microsoft.com>
2019-08-02 22:52:01 -0400

    fix issue with count not cast as int

--- vmScaleSetTest/vmScaleSetTest.ps1
+++ vmScaleSetTest/vmScaleSetTest.ps1
@@ -4,17 +4,13 @@ param(
     [Parameter(Mandatory=$True)][string] $autoScaleRuleName
 )
 
-Function Set-ScaleRuleCount ([string]$rgName, [string]$vmssName, [int] $n)
+Function Set-ScaleRuleCount ([string]$rgName, [string]$name, [int] $count)
 {
-    Write-Host "Setting autoscaling rule to require $($n) instances."
-    
-    # Edit autoscale rule to reflect new count
-    $out = az monitor autoscale update -g $rg.name --name $r.name --count $n
+    $out = az monitor autoscale update -g $rgName --name $name --count $count
 }
-Function Force-ScaleOperation ([int] $n)
+Function Force-ScaleOperation ([string]$rgName, [string]$vmssName, [int] $count)
 {
-    Write-Host "Forcing autoscaling event to require $($n) instances."
-    $out = az vmss scale -g $vmss.resourceGroup --name $vmss.name --new-capacity $n
+    $out = az vmss scale -g $rgName --name $vmssName --new-capacity $count
 }
 
 Function Get-InstanceCount ($resourceGroupName, $vmssName){
@@ -27,7 +23,7 @@ Function Wait-ScalingOperation ([int] $count){
     while ($bContinue){
         #$count =  ((az vmss list-instances -g $vmss.resourceGroup --name $vmss.name | ConvertFrom-Json) | Where-Object -Property provisioningState -eq "Succeeded").count
         $c =  ((az vmss list-instances -g $vmss.resourceGroup --name $vmss.name | ConvertFrom-Json)).count
-        $waitTime = 5
+        $waitTime = 15
         if ($c -ne $count){
             Write-Host "Current instance count is $($c) but waiting for $($count).  Waiting for $($waitTime) seconds."
             Start-Sleep $waitTime
@@ -48,48 +44,59 @@ Function Remove-VmssInstances ([string]$resourceGroupName, [string] $vmssName, [
         }        
     }
     else{
-        Write-Host "Cannot remove $($count) instance(s) from scalset $($vmssName) as it only has $($result.count) instance(s)."
+        Write-Host "Cannot remove $($count) instance(s) from scalset $($vmssName) as it has $($result.count) instance(s) in a Succeeded state." -ForegroundColor Red
+        exit
     }
 }
 
+$color = "Cyan"
 $vmss = az vmss show -g $resourceGroupName --name $vmssName| ConvertFrom-json
 
 # GET AUTOSCALE RULE
-#$rules = az monitor autoscale list -g $rg.name
 $r = az monitor autoscale show -g $vmss.resourceGroup --name $autoScaleRuleName | ConvertFrom-Json
+
+if ($null -eq $r){
+    Write-Host "Autoscale rule not found."
+    Exit
+}
+
 $autoscaleMin = $r.profiles.capacity.minimum
 $autoscaleStart = $r.profiles.capacity.minimum
 
-Write-Host "Autoscale minimum instance count is $($autoscaleMin)."
+Write-Host "Autoscale minimum instance count is $($autoscaleMin)." -ForegroundColor $color
 $iCount = Get-InstanceCount -resourceGroupName $resourceGroupName -vmssName $vmssName
-Write-Host "Current capacity for VM Scaleset $($vmss.name) is $($iCount)."
+Write-Host "Current capacity for VM Scaleset $($vmss.name) is $($iCount)." -ForegroundColor $color
 
 # WAIT FOR AUTOSCALE RULE TO FORCE A STEADY STATE TO TARGET
 if ($iCount -ne $autoscaleMin){
-    Write-Host "Waiting for autoscale rule to force instance count to $($autoscaleMin)."
+    Write-Host "Waiting for autoscale rule to force instance count to $($autoscaleMin)."  -ForegroundColor $color
     Wait-ScalingOperation -count ($autoscaleMin)
 }
 
 # SET THE NUMBER OF INSTANCES THAT SHOULD BE DELETED HERE
 $n = 1
-Write-Host "Deleting $($n) instance(s)."
-Remove-VmssInstances -resourceGroupName $vmss.resourceGroup -vmssName $vmss.name $n
+Write-Host "Deleting $($n) instance(s)." -ForegroundColor $color
+Remove-VmssInstances -resourceGroupName $vmss.resourceGroup -vmssName $vmss.name -count $n
 Wait-ScalingOperation -count ($autoscaleMin - $n)
 
 # WAIT FOR AUTOSCALE RULE TO FORCE COUNT BACK TO TARGET
-Write-Host "Waiting for autoscale rule to force instance count to $($autoscaleMin)."
+Write-Host "Waiting for autoscale rule to force instance count to $($autoscaleMin)." -ForegroundColor $color
 Wait-ScalingOperation -count ($autoscaleMin)
 
 # FORCE A SCALE OUT OPERATION
-Force-ScaleOperation ($autoscaleMin + 1)
-Wait-ScalingOperation -count ($autoscaleMin + 1)
+$n = ([int]$autoscaleMin + 1)
+Write-Host "Forcing autoscaling event to require $($n) instances." -ForegroundColor $color
+Force-ScaleOperation -rgName $vmss.resourceGroup -vmssName $vmss.name -count $n
+Wait-ScalingOperation -count $n
 
 # WAIT FOR AUTOSCALE RULE TO FORCE COUNT BACK TO TARGET
-Write-Host "Waiting for autoscale rule to force instance count to $($autoscaleMin)."
+Write-Host "Waiting for autoscale rule to force instance count to $($autoscaleMin)." -ForegroundColor $color
 Wait-ScalingOperation -count ($autoscaleMin)
 
 # CHANGE AUTOSCALE TARGET HIGHER
-Set-ScaleRuleCount -rgName $vmss.resourceGroup -vmssName $vmss.name -n ($autoscaleMin + 2)
+$n = ([int]$autoscaleMin + 2)
+Write-Host "Setting autoscaling rule to require $($n) instances."
+Set-ScaleRuleCount -rgName $vmss.resourceGroup -name $autoScaleRuleName -count $n
 
 # REFRESH AUTOSCALE OBJECT AND GET NEW MIN VALUE
 $r = az monitor autoscale show -g $vmss.resourceGroup --name $autoScaleRuleName | ConvertFrom-Json
@@ -99,14 +106,25 @@ $autoscaleMin = $r.profiles.capacity.minimum
 Wait-ScalingOperation -count $autoscaleMin
 
 # UPDATE AUTOSCALE RULE TO 1 INSTANCE
-Set-ScaleRuleCount -rgName $vmss.resourceGroup -vmssName $vmss.name -n 1
+Write-Host "Setting autoscaling rule to require 1 instance."
+Set-ScaleRuleCount -rgName $vmss.resourceGroup -name $autoScaleRuleName -count 1
 # REFRESH AUTOSCALE OBJECT AND GET NEW MIN VALUE
 $r = az monitor autoscale show -g $vmss.resourceGroup --name $autoScaleRuleName | ConvertFrom-Json
 $autoscaleMin = $r.profiles.capacity.minimum
 
 # WAIT FOR AUTOSCALE RULE TO FORCE COUNT BACK TO TARGET
-Wait-ScalingOperation -count $autoscaleMin
+Wait-ScalingOperation -count [int]$autoscaleMin
 
 $vmss = az vmss show -g $vmss.resourceGroup --name $vmssName| ConvertFrom-json
-Write-Host "Current capacity for VM Scaleset $($vmss.name) is $($vmss.sku.capacity)."
-Write-Host "Setting autoscale rule back to staring minimum value of $($autoscaleStart)."
+Write-Host "Current capacity for VM Scaleset $($vmss.name) is $($vmss.sku.capacity)." -ForegroundColor $color
+
+# SET AUTOSCALE RULE MIN BACK TO STARTING VALUE
+Write-Host "Setting autoscale rule back to staring minimum value of $($autoscaleStart)." -ForegroundColor $color
+Set-ScaleRuleCount -rgName $vmss.resourceGroup -name $autoScaleRuleName -count [int]$autoscaleStart
+
+# REFRESH AUTOSCALE OBJECT AND GET NEW MIN VALUE
+$r = az monitor autoscale show -g $vmss.resourceGroup --name $autoScaleRuleName | ConvertFrom-Json
+$autoscaleMin = $r.profiles.capacity.minimum
+
+# WAIT FOR AUTOSCALE RULE TO FORCE COUNT BACK TO TARGET
+Wait-ScalingOperation -count [int]$autoscaleMin
