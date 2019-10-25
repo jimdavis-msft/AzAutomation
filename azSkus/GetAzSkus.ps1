1e42399a49de99ddcc9ae87a6cec900569db3aed
jimdavis-msft <jimdavis@microsoft.com>
2019-10-25 15:39:28 -0400

    added azSkus

---
+++ azSkus/GetAzSkus.ps1
@@ -0,0 +1,33 @@
+﻿$skus = az vm list-skus | ConvertFrom-Json
+$eastskus = $skus | Where-Object -Property locations -EQ eastus
+$centralskus = $skus | Where-Object -Property locations -EQ centralus
+
+foreach ($_ in $eastskus)
+{
+    Write-Host "$($_.name)"
+    
+    foreach ($c in $_.capabilities)
+    {
+        if ($c.name -eq "AcceleratedNetworkingEnabled")
+        {
+            Write-Host "    $($c.name) : $($c.value) "
+        }
+    }
+}
+
+Write-Host ""
+Write-Host "***** centralus *****"
+Write-Host ""
+
+foreach ($_ in $centralskus)
+{
+    Write-Host "$($_.name)"
+    
+    foreach ($c in $_.capabilities)
+    {
+        if ($c.name -eq "AcceleratedNetworkingEnabled")
+        {
+            Write-Host "    $($c.name) : $($c.value) "
+        }
+    }
+}
