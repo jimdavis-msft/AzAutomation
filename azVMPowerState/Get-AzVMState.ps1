ce0321c6d80b4c095ab2ff0d34577cb70cc57fa3
Jim Davis <jimdavis@microsoft.com>
2020-01-16 08:34:21 -0500

    add example power state

---
+++ azVMPowerState/Get-AzVMState.ps1
@@ -0,0 +1,13 @@
+﻿$context = Get-AzContext
+
+if ($null -eq $context)
+{
+    $a = Connect-AzAccount
+}
+
+$v = Get-AzVM -Status
+
+foreach ($_ in $v)
+{
+    Write-Host "$($_.Name) == $($_.Powerstate)"
+}
